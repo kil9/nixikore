9817904717912a6eea733ebb5455cc9994edb6cc
kil9 <chungha.kim@navercorp.com>
2018-12-31 08:38:17 +0900

    Add numbered literals

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import re
 import random
+from enum import Enum, auto
 
 from sqlalchemy import or_
 import tossi
@@ -9,57 +10,98 @@ from model import Word
 from category import Categories
 
 
+class LiteralType(Enum):
+    NORMAL = auto()
+    MATCH_ALL = auto()
+    NUMBERS = auto()
+
+
 class Literal():
     def __init__(self, literal, session):
+        # self.regex_literal = r'%{\w*?}'
+        self.regex_matchall = r'%{{\w*?}}'
+        self.regex_numbers = r'%{(?P<start>\d*?)\-(?P<end>\d*?)}'
+
         self.content = literal
-        self.match_all = literal.startswith('%{{')
+
+        if re.match(self.regex_matchall, literal):
+            self.type = LiteralType.MATCH_ALL
+        elif re.match(self.regex_numbers, literal):
+            self.type = LiteralType.NUMBERS
+        else:
+            self.type = LiteralType.NORMAL
+
         self.category = literal.replace('%', '').replace('{', '').replace('}', '')
         self.session = session
 
-    def replace(self):
-        if not self.session:
-            log.error('no session')
-            return self.content
-
+    def _process_matchall(self) -> Word:
         session = self.session
-        if self.match_all:
-            all_categories = Categories.all_children(Categories.find_node(self.category))
-            count = session.query(Word).filter(
-                    or_(Word.category == c for c in all_categories)).count()
-            if count == 0:
-                log.error(f'count: 0 for category {all_categories}')
-                raise Exception('no entry for category')
-            ind = random.randrange(0, count)
-            cat = session.query(Word).filter(
-                    or_(Word.category == c for c in all_categories))[ind]
-        else:
-            count = session.query(Word).filter_by(category=self.category).count()
-            if count == 0:
-                log.error(f'count: 0 for category {self.category}')
-                raise Exception(f'no entry for category {self.category}')
-            ind = random.randrange(0, count)
-            cat = session.query(Word).filter_by(category=self.category)[ind]
+        all_categories = Categories.all_children(Categories.find_node(self.category))
+        count = session.query(Word).filter(
+                or_(Word.category == c for c in all_categories)).count()
+        if count == 0:
+            log.error(f'count: 0 for category {all_categories}')
+            raise Exception('no entry for category')
+        ind = random.randrange(0, count)
+        replaced = session.query(Word).filter(
+                or_(Word.category == c for c in all_categories))[ind]
+        return replaced
 
-        rep = replace_literals(cat.contents, session)
+    def _process_numbers(self) -> Word:
+        matched = re.match(self.regex_numbers, self.content)
+        start, end = matched.group('start'),matched.group('end')
+        rand = random.randrange(int(start), int(end))
+        return Word(contents=str(rand))
+
+    def _process_normal(self) -> Word:
+        session = self.session
+        count = session.query(Word).filter_by(category=self.category).count()
+        if count == 0:
+            log.error(f'count: 0 for category {self.category}')
+            raise Exception(f'no entry for category {self.category}')
+        ind = random.randrange(0, count)
+        replaced = session.query(Word).filter_by(category=self.category)[ind]
+        return replaced
+
+
+    def replace(self):
+        if self.type == LiteralType.MATCH_ALL:
+            replaced = self._process_matchall()
+        elif self.type == LiteralType.NUMBERS:
+            replaced = self._process_numbers()
+        else:
+            replaced = self._process_normal()
+
+        rep = replace_literals(replaced.contents, self.session)
         self.content = rep
         return self.content
 
 
 def determine_particle(word: str, rep: str) -> str:
     particle = word.replace(rep, '')
-    if '(' not in particle:
+    if not particle.startswith('('):
+        return word
+
+    if '()' in particle:
         return word
 
     if '%' in word:
         return word
 
-    ret = tossi.postfix(rep, particle)
-    log.error(f'{rep} / {particle} -> {ret}')
+    parsed = tossi.parse(particle)
+    log.error(f'particle: {particle}, parsed: {parsed}')
+
+    try:
+        ret = tossi.postfix(rep, particle)
+    except ValueError as e:
+        log.exception(e)
+        return word
+    log.debug(f'tossi: {rep} / {particle} -> {ret}')
     return ret
 
 
 def find_literals(script: str):
-    regex_literal = r'%{\w*?}|%{{\w*?}}'
+    regex_literal = r'%{\w*?}|%{{\w*?}}|%{\d*?\-\d*?}'
     return re.findall(regex_literal, script)
 
 
@@ -68,31 +110,46 @@ def replace_literals(script, session):
     if not literals:
         return script
 
-    for literal in literals:
+    for i, literal in enumerate(literals):
         try:
             rep = Literal(literal, session).replace()
-        except Exception:
+        except Exception as e:
+            log.exception(e)
             log.error(f'failed to replace literal: {literal}')
             return script
         log.debug(f'literal: {literal} -> {rep}')
 
         words = script.split(' ')
-        log.error(f'words1: {words}')
+        log.info(f'words1: {words}')
         for i, word in enumerate(words):
             if literal in word:
                 word = word.replace(literal, rep, 1)
                 word = determine_particle(word, rep)
                 words[i] = word
                 break
-        log.error(f'words2: {words}')
+
+        log.info(f'words2: {words}')
         script = ' '.join(words)
+
+        numbered_literal = '%{' + str(i+1) + '}'
+        if numbered_literal in script:
+            script = script.replace(numbered_literal, rep)
+        log.info(f'words3: {script}')
 
     return script
 
 
+def process_linefeed(script: str) -> str:
+    return script.replace('\\n', '\n')
+
+
 def compile_script(script: str, image_keyword=None):
     with session_scope() as session:
-        log.info(f'script: {script}')
+        log.info(f'raw script: {script}')
+
         script = replace_literals(script, session)
+        log.info(f'replaced script: {script}')
+
+        script = process_linefeed(script)
         log.info(f'final script: {script}')
         return script
--- test_script.py
+++ test_script.py
@@ -26,8 +26,11 @@ class ScriptTestCase(unittest.TestCase):
     def test_find_literals(self):
         script = '%{사람 %{사람} } %{랜덤} literals'
         literals = find_literals(script)
-
         self.assertEqual(literals, ['%{사람}', '%{랜덤}'])
+
+        script = '%{1000-2030}'
+        literals = find_literals(script)
+        self.assertEqual(literals, ['%{1000-2030}'])
 
     def test_find_literals_matchall(self):
         script = '%{{사람}} literals'
@@ -38,5 +41,23 @@ class ScriptTestCase(unittest.TestCase):
     def test_particles(self):
         script = '%{{사람}}(이)가 어쨌다고요'
         script = compile_script(script, self.session)
-
         self.assertFalse('(' in script)
+
+        script = '서울 대표(%{1900-1901})'
+        script = compile_script(script, self.session)
+        self.assertEqual(script, '서울 대표(1900)')
+
+        script = '서울 대표(%{1900-1901}-%{1949-1950})'
+        script = compile_script(script, self.session)
+        self.assertEqual(script, '서울 대표(1900-1949)')
+
+        script = '서울 대표(%{장소})'
+        script = compile_script(script, self.session)
+        self.assertTrue(script.startswith('서울 대표('))
+        self.assertTrue(script.endswith(')'))
+
+    def test_numbered_literal(self):
+        script = '%{사람}%{1}'
+        script = compile_script(script, self.session)
+        split = int(len(script)/2)
+        self.assertEqual(script[:split], script[split:])
